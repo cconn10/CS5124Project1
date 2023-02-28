2d85b29dbba78e987f2a40ff6512247d59074443
Colin Conn <conncf@mail.uc.edu>
2023-02-27 22:59:48 -0500

    Finished B-goals

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -1,15 +1,15 @@
 class Scatterplot {
     
-    constructor(_config, _data, _discriminator) {
+    constructor(_config, _data) {
         this.config = {
             parentElement: _config.parentElement,
-            containerWidth: _config.containerWidth || 200,
-            containerHeight: _config.containerHeight || 200,
-            margin: { top: 5, right: 10, bottom: 30, left: 50 }
+            containerWidth: _config.containerWidth || 800,
+            containerHeight: _config.containerHeight || 275,
+            margin: { top: 25, right: 10, bottom: 40, left: 90 },
+            tooltipPadding: _config.tooltipName || 15
         }
 
         this.data = _data
-        this.discriminator = _discriminator
 
         this.initVis()
     }
@@ -25,11 +25,11 @@ class Scatterplot {
             .attr('height', vis.config.containerHeight)
             .append('g')
                 .attr('transform', `translate(${vis.config.margin.left}, ${vis.config.margin.top})`)
-
-        vis.xScale = d3.scaleLinear()
+        
+        vis.xScale = d3.scaleLog()
             .range([0, vis.width])
 
-        vis.yScale = d3.scaleLinear()
+        vis.yScale = d3.scaleLog()
             .range([vis.height, 0])
 
         vis.xAxis = d3.axisBottom(vis.xScale)
@@ -43,18 +43,58 @@ class Scatterplot {
             .attr('class', 'axis y-axis')
 
         
+        vis.chart.append('text')
+            .attr('class', 'axis-title')
+            .attr('y', vis.height + 30)
+            .attr('x', vis.config.containerWidth / 2)
+            .attr('dy', '.71em')
+            .style('text-anchor', 'end')
+            .text("Radius (Earth Radius)")
+        
+        vis.chart.append('text')
+            .attr('class', 'axis-title')
+            .attr('y', -50)
+            .attr('x', -vis.height + 70)
+            .attr('dy', '.71em')
+            .attr('transform', `rotate(-90)`)
+            .text("Mass (Earth Mass)")
+
+        vis.chart.append('text')
+            .attr('class', 'chart-title')
+            .attr('y', -20)
+            .attr('x', 200)
+            .attr('dy', '.71em')
+            .text("Exoplanet Mass Related to Radius")
+
     }
 
     updateVis() {
         let vis = this
 
-        console.log(vis.data)
+        vis.values = vis.data.map(d => {return {
+            pl_name: d.pl_name,
+            pl_rade: d.pl_rade != 0 ? d.pl_rade : 0.01,
+            pl_bmasse: d.pl_bmasse != 0 ? d.pl_bmasse : 0.01,
+            color: d.pl_bmasse == 0 || d.pl_rade == 0 ? '#230C33' :  '#4FB062'
+        }})
 
+        let ourSolarSystem = [
+            {pl_name: "Mercury", pl_rade: 0.1915, pl_bmasse: 0.0553, color: '#E86A92'},
+            {pl_name: "Venus", pl_rade: 0.4745, pl_bmasse: 0.815, color: '#E86A92'},
+            {pl_name: "Earth", pl_rade: 1, pl_bmasse: 1, color: '#E86A92'},
+            {pl_name: "Mars", pl_rade: 0.0535, pl_bmasse: 0.107, color: '#E86A92'},
+            {pl_name: "Jupiter", pl_rade: 5.605, pl_bmasse: 317.8, color: '#E86A92'},
+            {pl_name: "Saturn", pl_rade: 47.6, pl_bmasse: 95.2, color: '#E86A92'},
+            {pl_name: "Uranus", pl_rade: 2.005, pl_bmasse: 14.5, color: '#E86A92'},
+            {pl_name: "Neptune", pl_rade: 1.94, pl_bmasse: 17.1, color: '#E86A92'}
+        ]
+        ourSolarSystem.forEach(planet => vis.values.push(planet))
+        
         vis.xValue = d => d.pl_rade
         vis.yValue = d => d.pl_bmasse
 
-        vis.xScale.domain([0, d3.max(vis.data, d => vis.xValue(d))]).nice()
-        vis.yScale.domain([0, d3.max(vis.data, d => vis.yValue(d))]).nice()
+        vis.xScale.domain([0.01, d3.max(vis.values, d => vis.xValue(d))]).nice()
+        vis.yScale.domain([0.01, d3.max(vis.values, d => vis.yValue(d))]).nice()
 
         vis.renderVis()
     }
@@ -63,13 +103,28 @@ class Scatterplot {
         let vis = this
         
         vis.chart.selectAll('.point')
-            .data(vis.data)
+            .data(vis.values)
             .join('circle')
                 .attr('class', 'point')
-                .attr('fill', 'steelblue')
-                .attr('cy', d => vis.yScale(vis.yValue(d)) )
+                .attr('fill', d => d.color)
+                .attr('opacity', '0.7')
+                .attr('cy', d => vis.yScale(vis.yValue(d)))
                 .attr('cx', d => vis.xScale(vis.xValue(d)))
-                .attr('r', 4)
+                .attr('r', 3)
+                .on('mouseover', (event, d) => {
+                    d3.select("#tooltip")
+                        .style('display', 'block')
+                        .style('left', (event.pageX + vis.config.tooltipPadding) + 'px')
+                        .style('top', (event.pageY + vis.config.tooltipPadding) + 'px')
+                        .html(`
+                        <p>Planet: ${d.pl_name}</p>
+                        <p>Radius: ${vis.xValue(d)} </p>
+                        <p>Mass: ${vis.yValue(d)}</p>
+                        `)
+                }).on('mouseout', (event, d) => {
+                    d3.select("#tooltip")
+                        .style('display', 'none')
+                })
                 
         vis.xAxisGroup.call(vis.xAxis)
         vis.yAxisGroup.call(vis.yAxis)
--- js/splitbarchart.js
+++ js/splitbarchart.js
@@ -3,9 +3,10 @@ class SplitBarChart {
     constructor(_config, _data) {
         this.config = {
             parentElement: _config.parentElement,
-            containerWidth: _config.containerWidth || 200,
-            containerHeight: _config.containerHeight || 200,
-            margin: { top: 5, right: 10, bottom: 30, left: 50 }
+            containerWidth: _config.containerWidth || 325,
+            containerHeight: _config.containerHeight || 275,
+            margin: { top: 25, right: 10, bottom: 35, left: 60 },
+            tooltipPadding: _config.tooltipName || 15
         }
 
         this.data = _data
@@ -19,44 +20,40 @@ class SplitBarChart {
         vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right
         vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom
 
-        const STAR_TYPES = ["A", "F", "G", "K", "M"]
-        const BLANK_MSG = "[Blank]"
-
-        vis.mainGroupData = Array.from(d3.group(vis.data, d => STAR_TYPES.includes(d.st_spectype[0].toUpperCase()) 
-            ? d.st_spectype[0].toUpperCase() 
-            : BLANK_MSG)).sort()
-
-        vis.values = []
-
-        vis.groups = vis.mainGroupData.map(d => d[0])
-        vis.subgroups = ["Too Hot", "Habitable", "Too Cold", "Unknown"]
-
-        vis.mainGroupData.forEach(d => {
-            vis.values.push({group: d[0], subgroups: Array.from(d3.rollup(d[1], v => v.length, v => vis.habitableZone(d[0], v)))})
-        })
-
         vis.chart = d3.select(vis.config.parentElement)
             .attr('width', vis.config.containerWidth)
             .attr('height', vis.config.containerHeight)
             .append('g')
                 .attr('transform', `translate(${vis.config.margin.left}, ${vis.config.margin.top})`)
 
+
+            vis.mainGroupData = Array.from(d3.group(vis.data, d => STAR_TYPES.includes(d.st_spectype[0].toUpperCase()) 
+                ? d.st_spectype[0].toUpperCase() 
+                : BLANK_MSG)).sort()
+    
+            vis.values = []
+    
+            vis.groups = vis.mainGroupData.map(d => d[0])
+            vis.subgroups = ["Too Hot", "Habitable", "Too Cold", "Unknown"]
+    
+            vis.mainGroupData.forEach(d => {
+                vis.values.push({group: d[0], subgroups: Array.from(d3.rollup(d[1], v => v.length, v => vis.habitableZone(d[0], v)))})
+            })
         vis.xScale = d3.scaleBand()
             .domain(vis.groups)
             .range([0, vis.width])
             .paddingInner(0.15)
 
-        console.log(vis.xScale.bandwidth())
-
         vis.xSubScale = d3.scaleBand()
             .range([0, vis.xScale.bandwidth()])
             .domain(vis.subgroups)
+            .padding(0.25)
 
-        vis.yScale = d3.scaleLinear()
+        vis.yScale = d3.scaleLog()
             .range([vis.height, 0])
 
         vis.colorScale = d3.scaleOrdinal()
-            .range(["#9067C6", "#F4BFDB", "#ACFCD9", "#230C33"])
+            .range(["#748CAB", "#4FB062", "#E86A92", "#230C33"])
             .domain(vis.subgroups)
 
         vis.xAxis = d3.axisBottom(vis.xScale).tickSize(0)
@@ -68,8 +65,30 @@ class SplitBarChart {
         
         vis.yAxisGroup = vis.chart.append('g')
             .attr('class', 'axis y-axis')
-
         
+        vis.chart.append('text')
+            .attr('class', 'axis-title')
+            .attr('y', vis.height + 20)
+            .attr('x', vis.config.containerWidth / 2)
+            .attr('dy', '.71em')
+            .style('text-anchor', 'end')
+            .text("Star Type")
+        
+        vis.chart.append('text')
+            .attr('class', 'axis-title')
+            .attr('y', -50)
+            .attr('x', -vis.height + 70)
+            .attr('dy', '.71em')
+            .attr('transform', `rotate(-90)`)
+            .text("Count of Exoplanets")
+
+        vis.chart.append('text')
+            .attr('class', 'chart-title')
+            .attr('y', -20)
+            .attr('x', 40)
+            .attr('dy', '.71em')
+            .text("Habitable vs Non-habitable")
+
     }
 
     updateVis() {
@@ -78,8 +97,7 @@ class SplitBarChart {
         vis.xValue = d => d.subgroup
         vis.yValue = d => d.count   
 
-        vis.xScale
-        vis.yScale.domain([0, d3.max(vis.values.map(d => d3.max(d.subgroups, v => v[1])))]).nice()
+        vis.yScale.domain([0.1, d3.max(vis.values.map(d => d3.max(d.subgroups, v => v[1])))]).nice()
 
         vis.renderVis()
     }
@@ -87,25 +105,38 @@ class SplitBarChart {
     renderVis() {
         let vis = this
         
-        console.log(vis.xScale.bandwidth())
-
-        vis.chart
-            vis.chart.append('g')
-            .selectAll('g')
-            .data(vis.values)
-            .join("g")
-                .attr("transform", d => `translate(${vis.xScale(d.group)}, 0)`)
-                .selectAll(".bar")
-                    .data(d => vis.subgroups.map(function(key) { 
-                        return {subgroup: key, count: d.subgroups.some(v => v[0] == key) ? d.subgroups.filter(v => v[0] == key)[0][1] : 0}
-                    }))
-                    .join('rect')
-                        .attr('class', 'bar')
-                        .attr('fill', d => vis.colorScale(vis.yValue(d)))
-                        .attr('width', vis.xSubScale.bandwidth())
-                        .attr('height', d => vis.height - vis.yScale(vis.yValue(d)))
-                        .attr('y', d => vis.yScale(vis.yValue(d)))
-                        .attr('x', d => vis.xSubScale(vis.xValue(d)))
+        vis.chart.append('g')
+        .selectAll('g')
+        .data(vis.values)
+        .join("g")
+            .attr("transform", d => `translate(${vis.xScale(d.group)}, 0)`)
+            .selectAll(".bar")
+                .data(d => vis.subgroups.map(function(key) { 
+                    return {subgroup: key, count: d.subgroups.some(v => v[0] == key) ? d.subgroups.filter(v => v[0] == key)[0][1] : 0.1, group: d.group}
+                }))
+                .join('rect')
+                    .attr('class', 'bar')
+                    .attr('class', d => d.subgroup)
+                    .attr('fill', d => vis.colorScale(vis.xValue(d)))
+                    .attr('width', vis.xSubScale.bandwidth())
+                    .attr('height', d => vis.height - vis.yScale(vis.yValue(d)))
+                    .attr('y', d => vis.yScale(vis.yValue(d)))
+                    .attr('x', d => vis.xSubScale(vis.xValue(d)))
+                    .on('mouseover', (event, d) => {
+                        d3.select("#tooltip")
+                            .style('display', 'block')
+                            .style('left', (event.pageX + vis.config.tooltipPadding) + 'px')
+                            .style('top', (event.pageY + vis.config.tooltipPadding) + 'px')
+                            .html(`
+                            <p>Star Type: ${d.group}</p>
+                            <p>Is it Habitable?: ${d.subgroup}</p>
+                            <p>Count: ${vis.yValue(d)}</p>
+                            `)
+                    }).on('mouseout', (event, d) => {
+                        d3.select("#tooltip")
+                            .style('display', 'none')
+                    })
+    
                 
         vis.xAxisGroup.call(vis.xAxis)
         vis.yAxisGroup.call(vis.yAxis)
@@ -113,6 +144,8 @@ class SplitBarChart {
 
     habitableZone(group, dataPoint) {
         let value = dataPoint.pl_orbsmax
+        if(value == 0)
+            return "Unknown"
         switch(group.toUpperCase()) {
             case "A":
                 if(value < 8.5)
